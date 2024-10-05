7ec917824c3a604d21b2993054e320f4eb2b0469
basim-git <basim.asim321@gmail.com>
2024-10-05 16:02:34 +0500

    added main class to test library functions

---
+++ src/project_trial/main.java
@@ -0,0 +1,14 @@
+package project_trial;
+
+public class main {
+
+	public static void main(String[] args) 
+	{
+		Library myLibrary = new Library();
+		myLibrary.addBook("Harry Potter");
+		myLibrary.addBook("Lord of the Rings");
+		myLibrary.listBooks();
+
+	}
+
+}
